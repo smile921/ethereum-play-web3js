8b62876c9485636728d7e577318f3c4386530ddb
921.org <ruby2010ruby@163.com>
2017-04-01 14:49:09 +0800

    :hammer: fix   乘以7函数调试通了

--- config/environment.js
+++ config/environment.js
@@ -3,8 +3,9 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ethereum-play-web3js',
+    podModuePrefix:'ethereum-play-web3js/modules',
 	//web3Provider: 'http://localhost:8545',
-	web3Provider: 'http://10.88.1.78:8545',
+	web3Provider: 'http://10.88.1.22:8545',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
---
+++ tests/unit/helpers/contract-test.js
@@ -0,0 +1,12 @@
+
+import { contract } from 'ethereum-play-web3js/helpers/contract';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | contract');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = contract([42]);
+  assert.ok(result);
+});
+
